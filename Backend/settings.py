8d5e8a1ea2ac7b8a7252dbf3281d141882ff391c
pu <hpovleakhena0@gmail.com>
2026-01-26 08:33:40 +0700

    Fix CORS origin URL by removing trailing slash

--- Backend/settings.py
+++ Backend/settings.py
@@ -139,7 +139,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://127.0.0.1:8080",
     "http://127.0.0.1:8081",
-    "https://mini-mart-iota-seven.vercel.app/"
+    "https://mini-mart-iota-seven.vercel.app"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
